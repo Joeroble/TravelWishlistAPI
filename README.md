2809d09a9eb86f7022e4ed1f57338e3f4ca4d399
JRoble <89475216+Joeroble@users.noreply.github.com>
2022-02-26 11:23:25 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## Travel Wishlist Recycler View Example
+
+* Recycler View and Adapter
+* ViewModel
+* Sideways swipes to delete
+* Background when list items swiped
+* Re-order list items by dragging up and down
+* Interfaces
+* Map intent
+
+Background image credit: Photo by
+ <a href="https://unsplash.com/@danielroe?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+ Daniel Roe</a> on <a href="https://unsplash.com/s/photos/travel?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
---
+++ app/src/main/java/com/joeroble/android/travelwishlist/PlacesViewModel.kt
@@ -0,0 +1,52 @@
+package com.joeroble.android.travelwishlist
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+const val TAG = "PLACES_VIEW_MODEL"
+class PlacesViewModel: ViewModel() {
+
+    private val places = mutableListOf<Place>(Place("Auckland"), Place("Patagonia"))
+
+    fun getPlaces(): List<Place>{
+        return places // smart cast
+    }
+
+    fun addNewPlace(place: Place, position: Int? = null): Int{
+        //return location in the list that the new item was added
+
+//        for (placeName in placeNames){
+//            if (placeName.name.uppercase() == place.name.uppercase()){
+//                return -1 //-1
+//            }
+//        }
+
+        // all function returns true if all of the things in a list meet a condition
+        // any function returns true if any of the things in a list meed a condition
+        if(places.any {placeName -> placeName.name.uppercase() == place.name.uppercase()}){
+            return -1
+        }
+
+        return if (position == null){
+            places.add(place) // adds at the end
+            places.lastIndex
+        } else {
+            places.add(position, place)
+            position
+        }
+
+
+    }
+
+    fun movePlace(from: Int, to: Int){
+        val place = places.removeAt(from)
+        places.add(to, place)
+        Log.d(TAG, places.toString())
+
+    }
+
+    fun deletePlace(position: Int): Place{
+       return places.removeAt(position)
+    }
+
+}
