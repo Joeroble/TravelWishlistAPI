85e31b606d0bb3b7b26017f7eb197894333f6f1b
JRoble <89475216+Joeroble@users.noreply.github.com>
2022-03-29 09:42:08 -0500

    Help!

--- app/src/main/java/com/joeroble/android/travelwishlist/Place.kt
+++ app/src/main/java/com/joeroble/android/travelwishlist/Place.kt
@@ -3,8 +3,9 @@ package com.joeroble.android.travelwishlist
 import java.text.SimpleDateFormat
 import java.util.*
 
-class Place(val name: String, val reason: String? = null, val dateAdded: Date = Date(), var starred: Boolean = false, val id: Int? = null) {
-    fun formattedDate(): String{
-        return SimpleDateFormat("EEE, d, MMM yyyy",Locale.getDefault()).format(dateAdded)
-    }
+class Place(val name: String, val reason: String? = null,  var starred: Boolean = false, val id: Int? = null) {
+    //val dateAdded: Date? = Date(),
+//    fun formattedDate(): String{
+//        return SimpleDateFormat("EEE, d, MMM yyyy",Locale.getDefault()).format(dateAdded)
+//    }
 }
